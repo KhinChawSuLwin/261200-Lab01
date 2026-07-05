7d4fdd23a49f984f30d8a881c1b0920e31a6ad75
680615521KhinChawSuLwin <khinchawsulwin161@gmail.com>
2026-07-05 23:25:49 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Turtle july = new Turtle();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        july.bgcolor("light blue");
+
+        july.speed(10);
+        july.up();
+        july.setPosition(-120, 100);
+        july.down();
+        july.penColor("white");
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                july.forward(6);
+                july.left(10);
+            }
+            july.right(40);
+        }
+
+        july.up();
+        july.setPosition(-200, -150);
+        july.setDirection(0);
+        july.down();
+
+        for (int i = 0; i < 40; i++)
+        {
+            if (i % 3 == 0)
+            {
+                july.penColor("darkgreen");
+            } else if (i % 3 == 1) {
+                july.penColor("green");
+            } else {
+                july.penColor("forestgreen");
+            }
+
+            int bladeHeight = 60 + (int)(Math.random() * 40);
+            int swayAngle = 5 + (int)(Math.random() * 10);
+
+            july.left(90 - swayAngle);
+            july.forward(bladeHeight);
+            july.right(160);
+            july.forward(bladeHeight);
+            july.left(70 + swayAngle);
+            july.up();
+            july.forward(12);
+            july.down();
         }
     }
 }
